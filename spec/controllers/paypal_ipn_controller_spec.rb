9dfb759e69e157e4d8dcd3ce88268d6b0b17caf5
Bala Paranj <bparanj@gmail.com>
2012-06-29 21:24:29 -0700

    Cleaned up controller specs. The tests are slow

--- spec/controllers/paypal_ipn_controller_spec.rb
+++ spec/controllers/paypal_ipn_controller_spec.rb
@@ -11,38 +11,28 @@ describe PaypalIpnController do
     post 'notify', {}
     response.body.should be_blank
   end
-  # This is not the job of the controller anymore
-  specify 'Step 2 : Read post from Paypal' do
-    request.stub!(:raw_post).and_return(http_raw_data)
-    ipn = stub(:valid => true)
-    PaypalService.should_receive(:new).with(http_raw_data) { ipn }
+
+  specify 'Step 2 negative condition. If ipn is invalid, payment should not be processed' do
+  	paypal_service = double("PaypalService")
+  	paypal_service.stub(:valid => false)  	
+
+    PaypalService.should_not_receive(:process_payment)
 
     post 'notify', {}
   end
 
-  specify 'Step 2 negative condition : Read post from Paypal. If ipn is invalid, log the message for manual intervention' do
+  specify 'Step 2 negative condition. If ipn is invalid, log the message for manual intervention' do
+  	paypal_service = double("PaypalService")
+  	paypal_service.stub(:valid => false)  	
+
     logger = mock(:info => nil)
-    request.stub!(:raw_post).and_return(http_raw_data)
-    ipn = stub(:valid => false)
-    PaypalService.stub(:new).with(http_raw_data) { ipn }
     controller.stub!(:logger) { logger }
 
-    PaypalService.should_not_receive(:process_payment)
     logger.should_receive(:info)
 
     post 'notify', {}
   end
-
-  specify 'Use the transaction ID to verify that the transaction has not already been processed' do
-    request.stub!(:raw_post).and_return(http_raw_data)
-    @notify = Paypal::Notification.new(http_raw_data)
-    Paypal::Notification.stub(:new) { @notify }
-    Paypal::Notification.any_instance.stub(:ssl_post).and_return('VERIFIED')
-    PaypalService.should_receive(:process_payment)
-
-    post 'notify', {}
-  end
-
+ 
   specify "View should not be rendered for the Paypal IPN callback" do
     post 'notify', {}
     response.body.should be_blank
